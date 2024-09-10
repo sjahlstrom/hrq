596626e9e346c19b912556cafd0a5cfeb1d212a7
Steve Ahlstrom <steve@steveahlstrom.com>
2024-09-10 10:46:31 -0600

    fixed are are issue

--- components/Analysis/Data/LieScale.tsx
+++ components/Analysis/Data/LieScale.tsx
@@ -3,19 +3,19 @@ const lieScale = [
         low: 0,
         high: 100,
         analysis:
-            'One of the dimensions measured by this test is how well you wish to be perceived by others.   It also measures whether you tell the truth about yourself relative to your need to be seen positively.   On this dimension, your scores are low.   You are less honest, not acknowledging imperfections and general qualities that are not always exemplary, but are are very human.',
+            'One of the dimensions measured by this test is how well you wish to be perceived by others.   It also measures whether you tell the truth about yourself relative to your need to be seen positively.   On this dimension, your scores are low.   You are less honest, not acknowledging imperfections and general qualities that are not always exemplary, but are very human.',
     },
     {
         low: 101,
         high: 200,
         analysis:
-            'One of the dimensions measured by this test is how well you wish to be perceived by others.   It also measures whether you tell the truth about yourself relative to your need to be seen positively.   On this dimension, your scores are in the middle range.   You are sometimes more honest, sometimes not,  sometimes acknowledging, sometimes not acknowledging imperfections and general qualities that are not always exemplary, but are are very human.',
+            'One of the dimensions measured by this test is how well you wish to be perceived by others.   It also measures whether you tell the truth about yourself relative to your need to be seen positively.   On this dimension, your scores are in the middle range.   You are sometimes more honest, sometimes not,  sometimes acknowledging, sometimes not acknowledging imperfections and general qualities that are not always exemplary, but are very human.',
     },
     {
         low: 201,
         high: 300,
         analysis:
-            'One of the dimensions measured by this test is how well you wish to be perceived by others .  It also measures whether you tend to tell the truth about yourself relative to your need to be seen positively.   On this dimension, your scores are high.   You are more honest, acknowledging imperfections and general qualities that are not always exemplary, are are very human.',
+            'One of the dimensions measured by this test is how well you wish to be perceived by others .  It also measures whether you tend to tell the truth about yourself relative to your need to be seen positively.   On this dimension, your scores are high.   You are more honest, acknowledging imperfections and general qualities that are not always exemplary, are very human.',
     },
 ]
 
