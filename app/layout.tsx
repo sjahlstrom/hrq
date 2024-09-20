6d4f4d8cf277d7ad3eb44433d721ce2ac2d9d5df
Steve Ahlstrom <steve@steveahlstrom.com>
2024-09-20 17:35:27 -0600

    added a roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,14 @@ import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import ScrollToTop from '@/components/ScrollToTop'
 import '../styles/index.css'
+import { Roboto } from 'next/font/google'
 
-// app/layout.tsx or app/layout.jsx
-import { Roboto } from '@next/font/google';
-
-// Configure the font by specifying weights, subsets, etc.
 const roboto = Roboto({
-    subsets: ['latin'], // Include the subset for the font
-    weight: ['400', '500', '700'], // Choose the font weights you need
-    display: 'swap', // Set the font display to swap to prevent layout shift
-});
+    weight: ['400', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+})
+// app/layout.tsx or app/layout.jsx
 
 export default function RootLayout({
     children,
