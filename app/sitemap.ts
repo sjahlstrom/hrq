709053d8528cc37a53ea3024bb086637a325e466
Steve Ahlstrom <steve@steveahlstrom.com>
2024-10-23 15:48:24 -0600

    Added bioform, save and read from db, redirect non-logged in user to signin

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -12,7 +12,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
         '/tos',
         '/price',
         '/privacy',
-        '/bio',
+        '/cta',
     ]
 
     return routes.map((route) => ({
