6950381f793d6f46f23b002587098e6ab20fa107
Steve Ahlstrom <steve@steveahlstrom.com>
2024-11-24 16:04:42 -0700

    renamed admin to dashboard

--- components/Hero/auth-button.tsx
+++ components/Hero/auth-button.tsx
@@ -13,8 +13,8 @@ export default function AuthButton() {
 
     return (
         <div className="mt-auto w-full">
-            <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 w-full flex justify-center">
-                <Button asChild size="lg" variant="outline" className="border-4 rounded-xl font-extrabold w-1/2 text-[#D64E30]">
+            <div className="justify-center">
+                <Button asChild size="lg" variant="outline" className="border-4 rounded-xl font-extrabold w-full text-[#D64E30]">
                     <Link href="/sign-in">LET&apos;S GET STARTED</Link>
                 </Button>
             </div>
